2946404ecb146cc393907b5f761786753d4ef2c4
marius <marius.b.fehr@gmail.com>
2017-04-09 20:37:12 +0200

    try to support different imager formats

--- aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
+++ aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
@@ -35,21 +35,21 @@ class BagImageDatasetReader(object):
         # Get the message indices
         conx = self.bag._get_connections(topics=imagetopic)
         indices = self.bag._get_indexes(conx)
-        
+
         try:
             self.index = indices.next()
         except:
             raise RuntimeError("Could not find topic {0} in {1}.".format(imagetopic, self.bagfile))
-        
+
         self.indices = np.arange(len(self.index))
-        
+
         #sort the indices by header.stamp
         self.indices = self.sortByTime(self.indices)
-        
+
         #go through the bag and remove the indices outside the timespan [bag_start_time, bag_end_time]
         if bag_from_to:
             self.indices = self.truncateIndicesFromTime(self.indices, bag_from_to)
-    
+
     #sort the ros messegaes by the header time not message time
     def sortByTime(self, indices):
         self.timestamp_corrector = sm.DoubleTimestampCorrector()
@@ -61,7 +61,7 @@ class BagImageDatasetReader(object):
             if self.perform_synchronization:
                 self.timestamp_corrector.correctTimestamp(data.header.stamp.to_sec(), \
                                                           stamp.to_sec())
-        
+
         sorted_tuples = sorted(zip(timestamps, indices))
         sorted_indices = [tuple_value[1] for tuple_value in sorted_tuples]
         return sorted_indices
@@ -89,14 +89,14 @@ class BagImageDatasetReader(object):
         valid_indices = []
         for idx, timestamp in enumerate(timestamps):
              if timestamp>=(bagstart+bag_from_to[0]) and timestamp<=(bagstart+bag_from_to[1]):
-                 valid_indices.append(idx)  
+                 valid_indices.append(idx)
         sm.logWarn("BagImageDatasetReader: truncated {0} / {1} images.".format(len(indices)-len(valid_indices), len(indices)))
         return valid_indices
-    
+
     def __iter__(self):
         # Reset the bag reading
         return self.readDataset()
-    
+
     def readDataset(self):
         return BagImageDatasetReaderIterator(self, self.indices)
 
@@ -115,17 +115,17 @@ class BagImageDatasetReader(object):
         else:
             timestamp = acv.Time( data.header.stamp.secs, data.header.stamp.nsecs )
         if data._type == 'mv_cameras/ImageSnappyMsg':
-            if self.uncompress is None:
-                from snappy import uncompress
-                self.uncompress = uncompress
-            img_data = np.reshape(self.uncompress(np.fromstring(data.data, dtype='uint8')),\
+          if self.uncompress is None:
+            from snappy import uncompress
+            self.uncompress = uncompress
+          img_data = np.reshape(self.uncompress(np.fromstring(data.data, dtype='uint8')),\
                                   (data.height,data.width), order="C")
-            
-        else:
-            img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-
-        if img_data.ndim == 3:
-            img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
-
+        elif data.encoding == "16UC1":
+          image_16u = np.array(self.CVB.imgmsg_to_cv2(data));
+          img_data = (image_16u / 256).astype("uint8")
+        elif data.encoding == "8UC1":
+          img_data = np.array(self.CVB.imgmsg_to_cv2(data, desired_encoding="mono8"))
+        elif data.encoding == "8UC3":
+          img_data = np.array(self.CVB.imgmsg_to_cv2(data))
+          img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
         return (timestamp, img_data)
-     
