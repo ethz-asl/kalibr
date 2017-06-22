db6c3ab4135f963e17111ca21ab2b0ffecd6ff48
Fadri Furrer <ffurrer@gmail.com>
2017-06-22 08:51:06 -0700

    fix format

--- aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
+++ aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
@@ -108,24 +108,27 @@ class BagImageDatasetReader(object):
     def numImages(self):
         return len(self.indices)
 
-    def getImage(self,idx):
+    def getImage(self, idx):
         topic, data, stamp = self.bag._read_message(self.index[idx].position)
         if self.perform_synchronization:
-            timestamp = acv.Time(self.timestamp_corrector.getLocalTime(data.header.stamp.to_sec()))
+            timestamp = acv.Time(self.timestamp_corrector.getLocalTime(
+                data.header.stamp.to_sec()))
         else:
-            timestamp = acv.Time( data.header.stamp.secs, data.header.stamp.nsecs )
+            timestamp = acv.Time(data.header.stamp.secs,
+                                 data.header.stamp.nsecs)
         if data._type == 'mv_cameras/ImageSnappyMsg':
-          if self.uncompress is None:
-            from snappy import uncompress
-            self.uncompress = uncompress
-          img_data = np.reshape(self.uncompress(np.fromstring(data.data, dtype='uint8')),\
-                                  (data.height,data.width), order="C")
+            if self.uncompress is None:
+                from snappy import uncompress
+                self.uncompress = uncompress
+            img_data = np.reshape(self.uncompress(np.fromstring(
+                data.data, dtype='uint8')), (data.height, data.width), order="C")
         elif data.encoding == "16UC1" or data.encoding == "mono16":
-          image_16u = np.array(self.CVB.imgmsg_to_cv2(data));
-          img_data = (image_16u / 256).astype("uint8")
+            image_16u = np.array(self.CVB.imgmsg_to_cv2(data))
+            img_data = (image_16u / 256).astype("uint8")
         elif data.encoding == "8UC1" or data.encoding == "mono8":
-          img_data = np.array(self.CVB.imgmsg_to_cv2(data, desired_encoding="mono8"))
+            img_data = np.array(self.CVB.imgmsg_to_cv2(
+                data, desired_encoding="mono8"))
         elif data.encoding == "8UC3" or data.encoding == "rgb8":
-          img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-          img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+            img_data = np.array(self.CVB.imgmsg_to_cv2(data))
+            img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
         return (timestamp, img_data)
