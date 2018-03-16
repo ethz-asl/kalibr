9ae174cbcc74a74b272e31f6ae70c9db15cd31aa
Matthew Pitropov <matthewpitropov@gmail.com>
2018-03-16 10:52:26 -0400

    Small change to support bgr8 images

--- aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
+++ aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
@@ -134,7 +134,7 @@ class BagImageDatasetReader(object):
       img_data = (image_16u / 256).astype("uint8")
     elif data.encoding == "8UC1" or data.encoding == "mono8":
       img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-    elif data.encoding == "8UC3" or data.encoding == "rgb8":
+    elif data.encoding == "8UC3" or data.encoding == "rgb8" or data.encoding == "bgr8":
       img_data = np.array(self.CVB.imgmsg_to_cv2(data))
       img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
     elif data.encoding == "8UC4" or data.encoding == "bgra8":
@@ -142,5 +142,5 @@ class BagImageDatasetReader(object):
       img_data = cv2.cvtColor(img_data, cv2.COLOR_BGRA2GRAY)
     else:
       raise RuntimeError(
-          "Unsupported Image format '{}' (Supported are: 16UC1 / mono16, 8UC1 / mono8, 8UC3 / rgb8, 8UC4 / bgra8 and ImageSnappyMsg)".format(data.encoding));
+          "Unsupported Image format '{}' (Supported are: 16UC1 / mono16, 8UC1 / mono8, 8UC3 / rgb8 / bgr8, 8UC4 / bgra8 and ImageSnappyMsg)".format(data.encoding));
     return (timestamp, img_data)
