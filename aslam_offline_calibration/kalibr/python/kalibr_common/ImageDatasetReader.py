becb6fa4991cea2fab41e3478697b74f6dd5c08d
Jack Morrison <jackmorrison1@gmail.com>
2018-06-07 13:14:08 -0700

    Fixes accompanying RuntimeError.

--- aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
+++ aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
@@ -146,5 +146,5 @@ class BagImageDatasetReader(object):
       img_data = cv2.cvtColor(img_data, cv2.COLOR_BAYER_BG2GRAY)
     else:
       raise RuntimeError(
-          "Unsupported Image format '{}' (Supported are: 16UC1 / mono16, 8UC1 / mono8, 8UC3 / rgb8, 8UC4 / bgra8, bayer_rggb8 and ImageSnappyMsg)".format(data.encoding));
+          "Unsupported Image format '{}' (Supported are: 16UC1 / mono16, 8UC1 / mono8, 8UC3 / rgb8 / bgr8, 8UC4 / bgra8, bayer_rggb8 and ImageSnappyMsg)".format(data.encoding));
     return (timestamp, img_data)
