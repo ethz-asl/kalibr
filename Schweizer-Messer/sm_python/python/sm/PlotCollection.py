fb571fc6a4912a8e9c626613c69c370cd1cbfb53
Jeremy Thompson <jeremythompson.918@gmail.com>
2018-07-18 15:22:14 +0200

    Update matplotlib import statement in SW
    Changing from matplotlib.backends.backend_wxagg to from matplotlib.backends.backend_wx

--- Schweizer-Messer/sm_python/python/sm/PlotCollection.py
+++ Schweizer-Messer/sm_python/python/sm/PlotCollection.py
@@ -5,7 +5,7 @@ import wx
 import wx.aui
 import matplotlib as mpl
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as Canvas
-from matplotlib.backends.backend_wxagg import NavigationToolbar2Wx as Toolbar
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx as Toolbar
 import collections
 
 class PlotCollection:
