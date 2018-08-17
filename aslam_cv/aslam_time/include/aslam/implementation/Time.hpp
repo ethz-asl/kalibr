769d7c31e5290e457e1bdce1c46b38d48abf41fb
Kaju Bubanja <bubanja.kaju@gmail.com>
2018-08-17 13:12:44 +0200

    removed rounding

--- aslam_cv/aslam_time/include/aslam/implementation/Time.hpp
+++ aslam_cv/aslam_time/include/aslam/implementation/Time.hpp
@@ -45,7 +45,6 @@
 //#include <ros/exception.h>
 //#include <aslam/time.h>
 #include <boost/date_time/posix_time/posix_time.hpp>
-#include <math.h>
 
 /*********************************************************************
  ** Cross Platform Headers
@@ -151,7 +150,7 @@ boost::posix_time::ptime TimeBase<T, D>::toBoost() const {
 #if defined(BOOST_DATE_TIME_HAS_NANOSECONDS)
   return pt::from_time_t(sec) + pt::nanoseconds(nsec);
 #else
-  return pt::from_time_t(sec) + pt::microseconds(static_cast<int32_t>(round(nsec / 1000.0)));
+  return pt::from_time_t(sec) + pt::microseconds(static_cast<int32_t>(nsec / 1000.0));
 #endif
 }
 
