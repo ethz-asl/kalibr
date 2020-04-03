38bfcbc8c63483363f798e80a4b4026eb4b244f9
michaelpantic <michael.pantic@gmail.com>
2020-04-03 17:27:21 +0200

    Adhesion check disabled for cam==1

--- aslam_offline_calibration/kalibr/python/kalibr_camera_calibration/MulticamGraph.py
+++ aslam_offline_calibration/kalibr/python/kalibr_camera_calibration/MulticamGraph.py
@@ -84,8 +84,14 @@ class MulticamCalibrationGraph(object):
 #############################################################    
     #check if all cams are connected through observations
     def isGraphConnected(self):
-        #check if all vertices are connected
-        return self.G.adhesion()
+
+        if self.numCams == 1:
+            # Since igaph 0.8, adhesion correctly returns 0 for the non-connected one cam case.
+            #   which evaluates to false later on. So we skip the check and return true in the one camera case.
+            return True
+        else:
+            #check if all vertices are connected
+            return self.G.adhesion()
         
     #returns the list of cam_ids that share common view with the specified cam_id
     def getCamOverlaps(self, cam_id):
